b265b986b7bf1ea24e5a58c5f53e671d0492f8f1
KarpusKonstantin <60568968+KarpusKonstantin@users.noreply.github.com>
2020-04-06 14:24:42 +0300

    feat: first commit

---
+++ script.js
@@ -0,0 +1,754 @@
+const buttonArrayRow1 = [
+    {
+        code: 'Backquote',
+        rus_eng: {
+            noShift: ['ё','`'],
+            isShift: ['Ё','~'],
+            caps: ['Ё','~'],
+            shiftCaps: ['ё','`'],
+        },
+    },
+    {
+        code: 'Digit1',
+        rus_eng: {
+            noShift: ['1','1'],
+            isShift: ['!','!'],
+            caps: ['1','1'],
+            shiftCaps: ['!','!'],
+        },
+    },
+    {
+        code: 'Digit2',
+        rus_eng: {
+            noShift: ['2','2'],
+            isShift: ['"','@'],
+            caps: ['2','2'],
+            shiftCaps: ['"','@'],
+        },
+    },
+    {
+        code: 'Digit3',
+        rus_eng: {
+            noShift: ['3','3'],
+            isShift: ['№','#'],
+            caps: ['3','3'],
+            shiftCaps: ['№','#'],
+        },
+    },
+    {
+        code: 'Digit4',
+        rus_eng: {
+            noShift: ['4','4'],
+            isShift: [';','$'],
+            caps: ['4','4'],
+            shiftCaps: [';','$'],
+        },
+    },
+    {
+        code: 'Digit5',
+        rus_eng: {
+            noShift: ['5','5'],
+            isShift: ['%','%'],
+            caps: ['5','5'],
+            shiftCaps: ['%','%'],
+        },
+    },
+    {
+        code: 'Digit6',
+        rus_eng: {
+            noShift: ['6','6'],
+            isShift: [':','^'],
+            caps: ['6','6'],
+            shiftCaps: [':','^'],
+        },
+    },
+    {
+        code: 'Digit7',
+        rus_eng: {
+            noShift: ['7','7'],
+            isShift: ['?','&'],
+            caps: ['7','7'],
+            shiftCaps: ['?','&'],
+        },
+    },
+    {
+        code: 'Digit8',
+        rus_eng: {
+            noShift: ['8','8'],
+            isShift: ['*','*'],
+            caps: ['8','8'],
+            shiftCaps: ['*','*'],
+        },
+    },
+    {
+        code: 'Digit9',
+        rus_eng: {
+            noShift: ['9','9'],
+            isShift: ['(','('],
+            caps: ['9','9'],
+            shiftCaps: ['(','('],
+        },
+    },
+    {
+        code: 'Digit0',
+        rus_eng: {
+            noShift: ['0','0'],
+            isShift: [')',')'],
+            caps: ['0','0'],
+            shiftCaps: [')',')'],
+        },
+    },
+    {
+        code: 'Minus',
+        rus_eng: {
+            noShift: ['-','-'],
+            isShift: ['_','_'],
+            caps: ['-','-'],
+            shiftCaps: ['_','-'],
+        },
+    },
+    {
+        code: 'Equal',
+        rus_eng: {
+            noShift: ['=','='],
+            isShift: ['+','+'],
+            caps: ['=','='],
+            shiftCaps: ['+','+'],
+        },
+    },
+    {
+        code: 'Backspace',
+        rus_eng: {
+            noShift: ['Backspace','Backspace'],
+            isShift: ['Backspace','Backspace'],
+            caps: ['Backspace','Backspace'],
+            shiftCaps: ['Backspace','Backspace'],
+        },
+    },
+
+];
+
+const buttonArrayRow2 = [
+    {
+        code: 'Tab',
+        rus_eng: {
+            noShift: ['Tab','Tab'],
+            isShift: ['Tab','Tab'],
+            caps: ['Tab','Tab'],
+            shiftCaps: ['Tab','Tab'],
+        },
+    },
+    {
+        code: 'KeyQ',
+        rus_eng: {
+            noShift: ['й','q'],
+            isShift: ['Й','Q'],
+            caps: ['Й','Q'],
+            shiftCaps: ['й','q'],
+        },
+    },
+    {
+        code: 'KeyW',
+        rus_eng: {
+            noShift: ['ц','w'],
+            isShift: ['Ц','W'],
+            caps: ['Ц','W'],
+            shiftCaps: ['ц','w'],
+        },
+    },
+    {
+        code: 'KeyE',
+        rus_eng: {
+            noShift: ['у','e',],
+            isShift: ['У','E',],
+            caps: ['У','E',],
+            shiftCaps: ['у','e',],
+        },
+    },
+    {
+        code: 'KeyR',
+        rus_eng: {
+            noShift: ['к','r',],
+            isShift: ['К','R',],
+            caps: ['К','R',],
+            shiftCaps: ['к','r',],
+        },
+    },
+    {
+        code: 'KeyT',
+        rus_eng: {
+            noShift: ['е','t',],
+            isShift: ['Е','T',],
+            caps: ['Е','T',],
+            shiftCaps: ['е','t',],
+        },
+    },
+    {
+        code: 'KeyY',
+        rus_eng: {
+            noShift: ['н','y',],
+            isShift: ['Н','Y',],
+            caps: ['Н','Y',],
+            shiftCaps: ['н','y',],
+        },
+    },
+    {
+        code: 'KeyU',
+        rus_eng: {
+            noShift: ['г','u',],
+            isShift: ['Г','U',],
+            caps: ['Г','U',],
+            shiftCaps: ['г','u',],
+        },
+    },
+    {
+        code: 'KeyI',
+        rus_eng: {
+            noShift: ['ш','i',],
+            isShift: ['Ш','I',],
+            caps: ['Ш','I',],
+            shiftCaps: ['ш','i',],
+        },
+    },
+    {
+        code: 'KeyO',
+        rus_eng: {
+            noShift: ['щ','o',],
+            isShift: ['Щ','O',],
+            caps: ['Щ','O',],
+            shiftCaps: ['щ','o',],
+        },
+    },
+    {
+        code: 'KeyP',
+        rus_eng: {
+            noShift: ['з','p',],
+            isShift: ['З','P',],
+            caps: ['З','P',],
+            shiftCaps: ['з','p',],
+        },
+    },
+    {
+        code: 'BracketLeft',
+        rus_eng: {
+            noShift: ['х','['],
+            isShift: ['Х','{'],
+            caps: ['Х','['],
+            shiftCaps: ['х','{'],
+        },
+    },
+    {
+        code: 'BracketRight',
+        rus_eng: {
+            noShift: ['ъ',']'],
+            isShift: ['Ъ','}'],
+            caps: ['Ъ',']'],
+            shiftCaps: ['ъ','}'],
+        },
+    },
+    {
+        code: 'Backslash',
+        rus_eng: {
+            noShift: ['\\','\\'],
+            isShift: ['/','|'],
+            caps: ['\\','\\'],
+            shiftCaps: ['/','||'],
+        },
+    },
+    {
+        code: 'Delete',
+        rus_eng: {
+            noShift: ['Del','Del'],
+            isShift: ['Del','Del'],
+            caps: ['Del','Del'],
+            shiftCaps: ['Del','Del'],
+        },
+    },
+];
+
+const buttonArrayRow3 = [
+    {
+        code: 'CapsLock',
+        rus_eng: {
+            noShift: ['CapsLock','CapsLock'],
+            isShift: ['CapsLock','CapsLock'],
+            caps: ['CapsLock','CapsLock'],
+            shiftCaps: ['CapsLock','CapsLock'],
+        },
+    },
+    {
+        code: 'KeyA',
+        rus_eng: {
+            noShift: ['ф','a'],
+            isShift: ['Ф','A'],
+            caps: ['Ф','A'],
+            shiftCaps: ['ф','a'],
+        },
+    },
+    {
+        code: 'KeyS',
+        rus_eng: {
+            noShift: ['ы','s'],
+            isShift: ['Ы','S'],
+            caps: ['Ы','S'],
+            shiftCaps: ['ы','s'],
+        },
+    },
+    {
+        code: 'KeyD',
+        rus_eng: {
+            noShift: ['в','d',],
+            isShift: ['В','D',],
+            caps: ['В','D',],
+            shiftCaps: ['в','d',],
+        },
+    },
+    {
+        code: 'KeyF',
+        rus_eng: {
+            noShift: ['а','f',],
+            isShift: ['А','F',],
+            caps: ['А','F',],
+            shiftCaps: ['а','f',],
+        },
+    },
+    {
+        code: 'KeyG',
+        rus_eng: {
+            noShift: ['п','g',],
+            isShift: ['П','G',],
+            caps: ['П','G',],
+            shiftCaps: ['п','g',],
+        },
+    },
+    {
+        code: 'KeyH',
+        rus_eng: {
+            noShift: ['р','h',],
+            isShift: ['Р','H',],
+            caps: ['Р','H',],
+            shiftCaps: ['р','h',],
+        },
+    },
+    {
+        code: 'KeyJ',
+        rus_eng: {
+            noShift: ['о','j',],
+            isShift: ['О','J',],
+            caps: ['О','J',],
+            shiftCaps: ['о','j',],
+        },
+    },
+    {
+        code: 'KeyK',
+        rus_eng: {
+            noShift: ['л','k',],
+            isShift: ['Л','K',],
+            caps: ['Л','K',],
+            shiftCaps: ['л','k',],
+        },
+    },
+    {
+        code: 'KeyL',
+        rus_eng: {
+            noShift: ['д','l',],
+            isShift: ['Д','L',],
+            caps: ['Д','L',],
+            shiftCaps: ['д','l',],
+        },
+    },
+    {
+        code: 'Semicolon',
+        rus_eng: {
+            noShift: ['ж',';',],
+            isShift: ['Ж',':',],
+            caps: ['Ж',';',],
+            shiftCaps: ['ж',':',],
+        },
+    },
+    {
+        code: 'Quote',
+        rus_eng: {
+            noShift: ['э','\''],
+            isShift: ['Э','"'],
+            caps: ['Э','\''],
+            shiftCaps: ['э','"'],
+        },
+    },
+    {
+        code: 'Enter',
+        rus_eng: {
+            noShift: ['Enter','Enter'],
+            isShift: ['Enter','Enter'],
+            caps: ['Enter','Enter'],
+            shiftCaps: ['Enter','Enter'],
+        },
+    },
+];
+
+const buttonArrayRow4 = [
+    {
+        code: 'ShiftLeft',
+        rus_eng: {
+            noShift: ['Shift','Shift'],
+            isShift: ['Shift','Shift'],
+            caps: ['Shift','Shift'],
+            shiftCaps: ['Shift','Shift'],
+        },
+    },
+    {
+        code: 'KeyZ',
+        rus_eng: {
+            noShift: ['я','z'],
+            isShift: ['Я','Z'],
+            caps: ['Я','Z'],
+            shiftCaps: ['я','z'],
+        },
+    },
+    {
+        code: 'KeyX',
+        rus_eng: {
+            noShift: ['ч','x'],
+            isShift: ['Ч','X'],
+            caps: ['Ч','X'],
+            shiftCaps: ['ч','x'],
+        },
+    },
+    {
+        code: 'KeyC',
+        rus_eng: {
+            noShift: ['с','c',],
+            isShift: ['С','C',],
+            caps: ['С','C',],
+            shiftCaps: ['с','c',],
+        },
+    },
+    {
+        code: 'KeyV',
+        rus_eng: {
+            noShift: ['м','v',],
+            isShift: ['М','V',],
+            caps: ['М','V',],
+            shiftCaps: ['м','v',],
+        },
+    },
+    {
+        code: 'KeyB',
+        rus_eng: {
+            noShift: ['и','b',],
+            isShift: ['И','B',],
+            caps: ['И','B',],
+            shiftCaps: ['и','b',],
+        },
+    },
+    {
+        code: 'KeyN',
+        rus_eng: {
+            noShift: ['т','n',],
+            isShift: ['Т','N',],
+            caps: ['Т','N',],
+            shiftCaps: ['т','n',],
+        },
+    },
+    {
+        code: 'KeyM',
+        rus_eng: {
+            noShift: ['ь','m',],
+            isShift: ['Ь','M',],
+            caps: ['Ь','M',],
+            shiftCaps: ['ь','m',],
+        },
+    },
+    {
+        code: 'Comma',
+        rus_eng: {
+            noShift: ['б',',',],
+            isShift: ['Б','<',],
+            caps: ['Б',',',],
+            shiftCaps: ['б','<',],
+        },
+    },
+    {
+        code: 'Period',
+        rus_eng: {
+            noShift: ['ю','.',],
+            isShift: ['Ю','>',],
+            caps: ['Ю','.',],
+            shiftCaps: ['ю','>',],
+        },
+    },
+    {
+        code: 'Slash',
+        rus_eng: {
+            noShift: ['.','/',],
+            isShift: [',','?',],
+            caps: ['.','/',],
+            shiftCaps: [',','?',],
+        },
+    },
+    {
+        code: 'ArrowUp',
+        rus_eng: {
+            noShift: ['▲','▲'],
+            isShift: ['▲','▲'],
+            caps: ['▲','▲'],
+            shiftCaps: ['▲','▲'],
+        },
+    },
+    {
+        code: 'ShiftRight',
+        rus_eng: {
+            noShift: ['Shift','Shift'],
+            isShift: ['Shift','Shift'],
+            caps: ['Shift','Shift'],
+            shiftCaps: ['Shift','Shift'],
+        },
+    },
+];
+
+const buttonArrayRow5 = [
+    {
+        code: 'ControlLeft',
+        rus_eng: {
+            noShift: ['Ctrl','Ctrl'],
+            isShift: ['Ctrl','Ctrl'],
+            caps: ['Ctrl','Ctrl'],
+            shiftCaps: ['Ctrl','Ctrl'],
+        },
+    },
+    {
+        code: 'MetaLeft',
+        rus_eng: {
+            noShift: ['Win','Win'],
+            isShift: ['Win','Win'],
+            caps: ['Win','Win'],
+            shiftCaps: ['Win','Win'],
+        },
+    },
+    {
+        code: 'AltLeft',
+        rus_eng: {
+            noShift: ['Alt','Alt'],
+            isShift: ['Alt','Alt'],
+            caps: ['Alt','Alt'],
+            shiftCaps: ['Alt','Alt'],
+        },
+    },
+    {
+        code: 'Space',
+        rus_eng: {
+            noShift: [' ',' ',],
+            isShift: [' ',' ',],
+            caps: [' ',' ',],
+            shiftCaps: [' ',' ',],
+        },
+    },
+    {
+        code: 'AltRight',
+        rus_eng: {
+            noShift: ['Alt','Alt',],
+            isShift: ['Alt','Alt',],
+            caps: ['Alt','Alt',],
+            shiftCaps: ['Alt','Alt',],
+        },
+    },
+    {
+        code: 'ArrowLeft',
+        rus_eng: {
+            noShift: ['◄','◄',],
+            isShift: ['◄','◄',],
+            caps: ['◄','◄',],
+            shiftCaps: ['◄','◄',],
+        },
+    },
+    {
+        code: 'ArrowDown',
+        rus_eng: {
+            noShift: ['▼','▼',],
+            isShift: ['▼','▼',],
+            caps: ['▼','▼',],
+            shiftCaps: ['▼','▼',],
+        },
+    },
+    {
+        code: 'ArrowRight',
+        rus_eng: {
+            noShift: ['►','►',],
+            isShift: ['►','►',],
+            caps: ['►','►',],
+            shiftCaps: ['►','►',],
+        },
+    },
+    {
+        code: 'ControlRight',
+        rus_eng: {
+            noShift: ['Ctrl','Ctrl',],
+            isShift: ['Ctrl','Ctrl',],
+            caps: ['Ctrl','Ctrl',],
+            shiftCaps: ['Ctrl','Ctrl',],
+        },
+    },
+];
+
+const allKeyArray = buttonArrayRow1.concat(buttonArrayRow2, buttonArrayRow3, buttonArrayRow4, buttonArrayRow5);
+
+class V_keyboard {
+    constructor () {
+        this.lang = localStorage.getItem('lang') || 'eng';
+        this.textArea = document.createElement('textarea');
+    }
+
+    createButtonRow(keyboard, buttonArrayRow) {
+        /* Создаем первую строку кнопок */
+        const row1 = document.createElement('div');
+        row1.classList.add('row');
+        keyboard.append(row1);
+
+        for (let button  of buttonArrayRow) {
+            let key = document.createElement('div');
+            key.classList.add('key',button.code);
+            key.dataset.button = button.code;
+
+            row1.append(key);
+
+            let span_rus = document.createElement('span');
+            let span_eng = document.createElement('span');
+
+            if (this.lang === 'rus') {
+                span_rus.classList.add('rus');
+                span_eng.classList.add('eng', 'hidden');
+            } else {
+                span_rus.classList.add('rus', 'hidden');
+                span_eng.classList.add('eng');
+            }
+
+            key.append(span_eng);
+            key.append(span_rus);
+
+            const span_rus_var = `<span class="noShift hidden">${button.rus_eng.noShift[0]}</span>
+                                  <span class="isShift hidden">${button.rus_eng.isShift[0]}</span>
+                                  <span class="caps hidden">${button.rus_eng.caps[0]}</span>
+                                  <span class="shiftCaps hidden">${button.rus_eng.shiftCaps[0]}</span>`;
+
+            const span_eng_var = `<span class="noShift hidden">${button.rus_eng.noShift[1]}</span>
+                                  <span class="isShift">${button.rus_eng.isShift[1]}</span>
+                                  <span class="caps hidden">${button.rus_eng.caps[1]}</span>
+                                  <span class="shiftCaps hidden">${button.rus_eng.shiftCaps[1]}</span>`;
+
+            span_rus.innerHTML = span_rus_var;
+            span_eng.innerHTML = span_eng_var;
+
+            key.addEventListener('click', this.clickButton.bind(this));
+            key.addEventListener('mousedown', this.mouseDown.bind(this)); // Должен появляться класс active
+            //    key.addEventListener('mouseup', this.mouseUp.bind(this));
+
+        }
+
+    }
+
+    init() {
+        /* Создаем блоки */
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('wrapper');
+
+        document.body.append(wrapper);
+
+        this.textArea.classList.add('inputarea');
+        wrapper.append(this.textArea);
+
+        const keyboard = document.createElement('div');
+        keyboard.classList.add('keyboard');
+        wrapper.append(keyboard);
+
+        this.createButtonRow(keyboard, buttonArrayRow1);
+        this.createButtonRow(keyboard, buttonArrayRow2);
+        this.createButtonRow(keyboard, buttonArrayRow3);
+        this.createButtonRow(keyboard, buttonArrayRow4);
+        this.createButtonRow(keyboard, buttonArrayRow5);
+
+    }
+
+    clickButton (event) {
+        let keyCode;
+
+        if (!event.target.dataset.button) {
+            let parentDiv = event.target.closest('div');
+            keyCode = parentDiv.dataset.button;
+        } else {
+            keyCode = event.target.dataset.button;
+        }
+
+        switch (keyCode) {
+            case 'Backspace':
+//                    break
+            case 'Tab':
+//                    this.tabHandler()
+                break;
+            case 'Delete':
+//                    this.delHandler()
+                break;
+            case 'CapsLock':
+//                    this.capsHandler(event)
+                break;
+            case 'Enter':
+//                    this.enterHandler()
+                break;
+            case 'ShiftLeft':
+                this.shiftHandler(event)
+                break;
+            case 'ShiftRight':
+                this.shiftHandler(event)
+                break;
+            case 'ControlLeft':
+//                    this.arrowUpHandler()
+                break;
+            case 'MetaLeft':
+//                    this.ctrlHandler()
+                break;
+            case 'AltLeft':
+//                    this.winHandler()
+                break;
+            case 'AltRight':
+//                    this.altHandler()
+                break;
+            case 'ArrowDown':
+//                    this.arrowLeftHandler()
+                break;
+            case 'ArrowRight':
+//                    this.arrowDownHandler()
+                break;
+            case 'ControlRight':
+//                    this.arrowRightHandler()
+                break;
+            default:
+                this.printText(event)
+        }
+
+    }
+
+    printText(event) {
+        /* Метод печатает символ в memo */
+        const word = event.target.textContent;
+        const key_child_span = document.querySelectorAll(`.${event.target.dataset.button} span span `);
+
+        if (key_child_span.length > 0) {
+            /* Target DIV и значет мы ищем чего написать */
+            key_child_span.forEach((item) => {
+                if (!item.classList.contains('hidden')) {
+                    this.textArea.value = this.textArea.value + item.textContent;
+                }
+            });
+        } else {
+            /* Мы уже находимя на букве и можем ее писать */
+            this.textArea.value = this.textArea.value + event.target.textContent;
+        }
+
+    }
+}
+
+window.addEventListener('load', () => {
+   console.log('Загружаем клавиатуру');
+   let v_keyboard = new V_keyboard();
+
+   v_keyboard.init();
+});
